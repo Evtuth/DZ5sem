e2b0f84467a3d8ab8f4d9465a317eccbae6ad895
Vasiliy <evtuth@gmail.com>
2023-01-18 21:45:27 +0300

    added amount even numbers

---
+++ zadacha34/Program.cs
@@ -0,0 +1,34 @@
+﻿//задача 34: Задайте массив заполненный случайными
+//положительными трёхзначными числами. Напишите программу, которая 
+//покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2.
+
+int[] CreateArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
+    return array;
+}
+
+int EvenAmount(int[] array)
+{
+    int amount = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            amount += 1;
+        }
+    }
+    return amount;
+}
+
+int[] array = new int[12];
+array = CreateArray(array);
+Console.Write("Ваш массив случайных трёхзначных чисел: ");
+Console.WriteLine(string.Join(", ", array));
+
+int amount = EvenAmount(array);
+Console.Write($"Количество чётных элементов массива равно {amount} !!!");
